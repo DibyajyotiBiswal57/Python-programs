cc162d6cfbdc245bcc4f917d98aab215fab4dd9a
Dibyajyoti <dibbispersonal@gmail.com>
2025-01-07 17:57:54 +0530

    Add files via upload

---
+++ adding_subtraction_multiplication.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+sum = num1 + num2
+diff = num1 - num2
+diff2 = num2 - num1
+prod = num1 * num2
+print("The sum is:", sum)
+print("The difference is:", diff, "and", diff2)
+print("The product is:", prod)
