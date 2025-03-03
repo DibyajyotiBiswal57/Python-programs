3956735770f32ce4b6142253099cc52efc23b48b
Dibyajyoti <dibbispersonal@gmail.com>
2025-03-03 22:19:00 +0530

    Add files via upload

---
+++ simple_interest.py
@@ -0,0 +1,9 @@
+p=float(input("Enter the principal: "))
+r=float(input("Enter the rate of interest: "))
+t=float(input("Enter the time taken in years: "))
+
+si=p*r*t
+amt=si+p
+
+print(f"The simple on {p} for {t} years at {r}% is {si}.")
+print(f"The amount is {amt}.")
