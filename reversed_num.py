1768aa4731dad5a2807ac1b758d154fc80ccf351
Dibyajyoti <dibbispersonal@gmail.com>
2025-03-05 19:59:35 +0530

    add

---
+++ reversed_num.py
@@ -0,0 +1,10 @@
+print("This program takes a 3 digit number and reverses its digits.")
+num = int(input("Enter a number: "))
+
+if num < 100 or num > 999:
+    print("Invalid number.")
+else:
+    num1 = num // 100
+    num2 = (num % 100) // 10
+    num3 = num % 10
+    print(f"The reversed number is: {num3}{num2}{num1}")
---
+++ weird_operations2.py
@@ -0,0 +1,13 @@
+print("Weird operations")
+num=int(input("Enter a number: "))
+
+if num < 100 or num > 999:
+    print("Invalid number.")
+else:
+    num1 = num // 100
+    num2 = (num % 100) // 10
+    num3 = num % 10
+    add=num1+num2
+    mul=num1*num3
+    print(f"The sum of the first and second digits is: {add}")
+    print(f"The product of the first and second digits is: {mul}")
