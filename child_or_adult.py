8d955739b85343ecfef4ada4fb8f1d86539e83e4
Dibyajyoti <dibbispersonal@gmail.com>
2025-04-21 19:38:23 +0530

    update

---
+++ child_or_adult.py
@@ -0,0 +1,6 @@
+print("This program checks if a person is a child or an adult.")
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a child.")
