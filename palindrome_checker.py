28f3636f2a60d244e8b3c541355d581df9e3995a
Dibyajyoti <dibbispersonal@gmail.com>
2025-07-22 23:05:04 +0530

    Create palindrome_checker.py

---
+++ palindrome_checker.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a 3-digit number: "))
+if 100 >= num >= 999:
+    print("Error! You need to input a 3-digit number.")
+else:
+    digit1 = int(num/100)
+    digit3 = int(num % 10)
+    digit2 = int((num/10) % 10)
+    newnum = 100*digit3 + 10*digit2 +digit1
+    if newnum == num:
+        print(f"{num} is a palindrome.")
+    else:
+        print(f"{num} is not a palindrome.")
