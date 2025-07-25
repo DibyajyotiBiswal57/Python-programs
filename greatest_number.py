f80192cfcdf7f93bef21468848f81f95d62e09a7
Dibyajyoti <dibbispersonal@gmail.com>
2025-07-25 22:04:31 +0530

    add

---
+++ greatest_number.py
@@ -0,0 +1,7 @@
+#Q36
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+list1 = [num1, num2, num3]
+greatno = max(list1)
+print(f"{greatno} is the greatest number.")
---
+++ smallest_no.py
@@ -0,0 +1,5 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+list1 = [num1, num2]
+smallno = min(list1)
+print(f"The smallest number is {smallno}.")
